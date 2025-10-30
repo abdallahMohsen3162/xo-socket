cfac9dda51f11709bcceb6d6cb6c7cc20a7ab9e7
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2025-10-30 12:39:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const io = require("socket.io")(3000, {
-    cors: {
-        origin: "*"
-    }
-})
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const { Server } = require("socket.io");
 
-let mp = new Map()
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+let mp = new Map();
 let st = new Set();
 let games = new Map();
 
+// ====================================================
+// SOCKET LOGIC
+// ====================================================
 io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
     socket.on("adduser", random_id => {
-        st.add(socket.id)
+        st.add(socket.id);
         mp.set(random_id, socket.id);
-    })
+    });
 
     socket.on("sendRequest", data => {
-        console.log(data);
-        io.to(mp.get(data.to)).emit("takeRequest", [data.from, data.message, data.board])
-    })
+        console.log("Request:", data);
+        io.to(mp.get(data.to)).emit("takeRequest", [data.from, data.message, data.board]);
+    });
 
     socket.on("getallusers", () => {
-        let arr = [];
-        for (let [a, b] of mp) {
-            arr.push(a);
-        }
-        io.emit("responseallusers", arr)
-    })
+        let arr = Array.from(mp.keys());
+        io.emit("responseallusers", arr);
+    });
 
     socket.on("accept", (data) => {
-        console.log(data);
+        console.log("Accept:", data);
 
         let gameId = [data.from, data.to].sort().join('-');
-
         let G = [];
+
         for (let i = 0; i < data.board; i++) {
             G[i] = "";
             for (let j = 0; j < data.board; j++) {
@@ -47,8 +55,8 @@ io.on("connection", (socket) => {
         });
 
         console.log(`Game ${gameId} created:`, G);
-        io.to(mp.get(data.to)).emit("accepted", data)
-    })
+        io.to(mp.get(data.to)).emit("accepted", data);
+    });
 
     function validation(gameBoard) {
         let draw = true;
@@ -57,100 +65,36 @@ io.on("connection", (socket) => {
                 if (gameBoard[i][j] == '.') {
                     draw = false;
                 }
-                let str = "";
-                if (i + 2 < gameBoard.length) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i + 1][j];
-                    str += gameBoard[i + 2][j];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
+                let directions = [
+                    [[1, 0], [2, 0]], // down
+                    [[-1, 0], [-2, 0]], // up
+                    [[0, 1], [0, 2]], // right
+                    [[0, -1], [0, -2]], // left
+                    [[1, 1], [2, 2]], // down-right
+                    [[-1, -1], [-2, -2]], // up-left
+                    [[-1, 1], [-2, 2]], // up-right
+                    [[1, -1], [2, -2]] // down-left
+                ];
 
-                str = "";
-                if (i - 2 >= 0) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i - 1][j];
-                    str += gameBoard[i - 2][j];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
+                for (let d of directions) {
+                    let str = gameBoard[i][j];
+                    for (let [di, dj] of d) {
+                        let ni = i + di, nj = j + dj;
+                        if (ni >= 0 && ni < gameBoard.length && nj >= 0 && nj < gameBoard.length)
+                            str += gameBoard[ni][nj];
                     }
+                    if (str === "XXX" || str === "OOO") return str[0];
                 }
-                str = "";
-                if (j + 2 < gameBoard.length) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i][j + 1];
-                    str += gameBoard[i][j + 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
-                str = "";
-                if (j - 2 >= 0) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i][j - 1];
-                    str += gameBoard[i][j - 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
-                str = "";
-                if (i + 2 < gameBoard.length && j + 2 < gameBoard.length) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i + 1][j + 1];
-                    str += gameBoard[i + 2][j + 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
-                str = "";
-                if (i - 2 >= 0 && j - 2 >= 0) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i - 1][j - 1];
-                    str += gameBoard[i - 2][j - 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
-                str = "";
-                if (i - 2 >= 0 && j + 2 < gameBoard.length) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i - 1][j + 1];
-                    str += gameBoard[i - 2][j + 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
-                str = "";
-                if (i + 2 < gameBoard.length && j - 2 >= 0) {
-                    str += gameBoard[i][j];
-                    str += gameBoard[i + 1][j - 1];
-                    str += gameBoard[i + 2][j - 2];
-                    if (str == "XXX" || str == "OOO") {
-                        return str[0];
-                    }
-                }
-
             }
         }
-        if (draw == true) {
-            return "draw"
-        }
+        if (draw) return "draw";
         return '.';
     }
 
     socket.on("sendplay", (data) => {
-        console.log(data);
+        console.log("Play:", data);
 
-        // Create game ID for this pair of players
         let gameId = [data.from, data.to].sort().join('-');
-
-        // Get the specific game for these players
         let game = games.get(gameId);
 
         if (!game) {
@@ -158,41 +102,28 @@ io.on("connection", (socket) => {
             return;
         }
 
-        // Update the specific game board
         let G = game.board;
         let s = "";
         for (let j = 0; j < G[data.i].length; j++) {
-            if (j == data.j) {
-                s += data.char;
-            } else {
-                s += G[data.i][j];
-            }
+            s += (j === data.j) ? data.char : G[data.i][j];
         }
         G[data.i] = s;
 
-
-        games.set(gameId, {
-            ...game,
-            board: G
-        });
+        games.set(gameId, { ...game, board: G });
 
         let ret = validation(G);
-        if (ret != '.') {
+        if (ret !== '.') {
             io.to(mp.get(data.to)).emit("winner", { player: ret });
             io.to(mp.get(data.from)).emit("winner", { player: ret });
-
-
             games.delete(gameId);
         }
 
-        io.to(mp.get(data.to)).emit("update", { G: G, flg: true });
-        io.to(mp.get(data.from)).emit("update", { G: G, flg: false });
-    })
+        io.to(mp.get(data.to)).emit("update", { G, flg: true });
+        io.to(mp.get(data.from)).emit("update", { G, flg: false });
+    });
 
     socket.on("disconnect", () => {
-        console.log("xxxxxxxxxxx", socket.id);
-
-
+        console.log("Disconnected:", socket.id);
         for (let [gameId, game] of games) {
             if (game.players.includes(socket.id)) {
                 games.delete(gameId);
@@ -200,4 +131,30 @@ io.on("connection", (socket) => {
             }
         }
     });
-})
+});
+
+// ====================================================
+// BASIC HTTP ENDPOINT
+// ====================================================
+app.get("/", (req, res) => {
+    res.send("Socket.IO server is running and alive 🚀");
+});
+
+// ====================================================
+// KEEP SERVER AWAKE ON RENDER
+// ====================================================
+setInterval(() => {
+    https.get("https://xo-socket.onrender.com", (res) => {
+        console.log("Self-ping status:", res.statusCode);
+    }).on("error", (err) => {
+        console.error("Self-ping failed:", err.message);
+    });
+}, 10 * 60 * 1000); // every 10 minutes
+
+// ====================================================
+// START SERVER
+// ====================================================
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
